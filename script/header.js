cab202a6b05af7c8c02044f836de4af5cc88e591
hed3i <gumbuble42@gmail.com>
2025-09-10 23:07:35 +0300

    first commit

---
+++ script/header.js
@@ -0,0 +1,22 @@
+const BLOCK_COAL = Block.createSpecialType({
+	solid: true,
+	renderlayer: EBlockRenderLayer.BLEND,
+	destroytime: 5,
+	explosionres: 6.0,
+	lightopacity: 15,
+	translucency: 0.0
+});
+
+const translate = function(str, args) {
+	try {
+		str = Translation.translate(str);
+		if (args !== undefined) {
+			Array.isArray(args) || (args = [args]);
+			str = java.lang.String.format(str, args);
+		}
+		return "" + str;
+	} catch (e) {
+		Logger.Log("Aboba#translate: " + e, "ERROR");
+		return "" + str;
+	}
+};
